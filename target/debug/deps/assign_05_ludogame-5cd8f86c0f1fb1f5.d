e625c08bc60fbbcf03f94f0fb6a70858254846e9
muhammadjehanzaib <soulsniper155@gmail.com>
2024-09-25 15:36:10 +0500

    cargo.lock and targets are updated

---
+++ target/debug/deps/assign_05_ludogame-5cd8f86c0f1fb1f5.d
@@ -0,0 +1,5 @@
+/home/muhammadjehanzaib/Ludo-Game/target/debug/deps/assign_05_ludogame-5cd8f86c0f1fb1f5: src/main.rs
+
+/home/muhammadjehanzaib/Ludo-Game/target/debug/deps/assign_05_ludogame-5cd8f86c0f1fb1f5.d: src/main.rs
+
+src/main.rs:
